7e284f666364b1b0179b86358ebdfece0ba1b09f
Light3039 <holypaladin47@gmail.com>
2022-05-20 15:18:34 +0430

    Chapter 4: Rays, a Simple Camera, and Background

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,50 @@
-pub mod color;
 pub mod vec3;
-use crate::color::write_color;
 use crate::vec3::Vec3;
-
 use Vec3 as Point3;
 use Vec3 as Color;
 
+pub mod color;
+use crate::color::write_color;
+
+pub mod ray;
+use crate::ray::Ray;
+
+pub fn ray_color(ray: &Ray) -> Color {
+    let unit_dir: Vec3 = vec3::unit_vec(ray.dir);
+    let t = 0.5 * (unit_dir.y + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 0.9)
+}
+
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f64) / aspect_ratio) as i32;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * aspect_ratio;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in 0..image_height {
         eprint!("\rScanlines remaining: {} ", image_height - j);
         for i in 0..image_width {
-            let r = (i as f64) / ((image_width - 1) as f64);
-            let g = (j as f64) / ((image_height - 1) as f64);
-            let b = 0.25;
-
-            let color: Color = Color::new(
-                (i as f64) / (image_height) as f64,
-                (j as f64) / (image_width) as f64,
-                0.25,
+            let u = (i as f64) / ((image_width - 1) as f64);
+            let v = (j as f64) / ((image_height - 1) as f64);
+            let ray = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
 
-            write_color(color);
+            write_color(ray_color(&ray));
         }
     }
 }
---
+++ src/ray.rs
@@ -0,0 +1,21 @@
+use crate::vec3::Vec3;
+use Vec3 as Point3;
+use Vec3 as Color;
+
+pub struct Ray {
+    pub orig: Point3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(orig: Point3, dir: Vec3) -> Ray {
+        Ray {
+            orig: orig,
+            dir: dir,
+        }
+    }
+
+    pub fn at(self, t: f64) -> Point3 {
+        self.orig + t * self.dir
+    }
+}
