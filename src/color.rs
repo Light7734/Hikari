ef31a6e517d25ebb876a0c210d1df87fb72d01c4
light7734 <mail@light7734.com>
2022-12-09 21:27:00 +0330

    Chapter 7: Antialiasing

--- src/color.rs
+++ src/color.rs
@@ -2,11 +2,13 @@ use crate::vec3::Vec3;
 
 use Vec3 as Color;
 
-pub fn write_color(color: Color) {
+pub fn write_color(color: Color, sample_count: i32) {
+    let scale = 1.0 / sample_count as f64;
+
     print!(
         "{} {} {}\n",
-        (255.999 * color.x) as i32,
-        (255.999 * color.y) as i32,
-        (255.999 * color.z) as i32,
+        (256.0 * f64::clamp(color.x * scale, 0.0, 0.99)) as i32,
+        (256.0 * f64::clamp(color.y * scale, 0.0, 0.99)) as i32,
+        (256.0 * f64::clamp(color.z * scale, 0.0, 0.99)) as i32,
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+pub mod camera;
+pub mod color;
 pub mod hittable;
+pub mod ray;
 pub mod scene;
 pub mod vec3;
 
@@ -6,31 +9,23 @@ use crate::vec3::Vec3;
 use Vec3 as Point3;
 use Vec3 as Color;
 
-pub mod color;
 use crate::color::write_color;
 
-pub mod ray;
 use crate::ray::Ray;
 
-use crate::hittable::{Hittable, Sphere};
+use crate::camera::Camera;
+use crate::hittable::Sphere;
 use crate::scene::Scene;
+
+use rand::distributions::{Distribution, Uniform};
 
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 1000;
     let image_height = ((image_width as f64) / aspect_ratio) as i32;
 
-    // Camera
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * aspect_ratio;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new();
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
@@ -48,17 +43,24 @@ fn main() {
         radius: 100.0,
     }));
 
-    for j in (0..image_height).rev() {
-        eprint!("\rScanlines remaining: {} ", image_height - j);
-        for i in 0..image_width {
-            let u = (i as f64) / ((image_width - 1) as f64);
-            let v = (j as f64) / ((image_height - 1) as f64);
-            let ray = Ray::new(
-                origin,
-                (u * horizontal) + (v * vertical) + lower_left_corner - origin,
-            );
+    let sample_count = 100;
+    let between = Uniform::from(0.0..1.0);
+    let mut rng = rand::thread_rng();
 
-            write_color(scene.process_ray(&ray, 0.0, f64::INFINITY));
+    for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        for i in 0..image_width {
+            let mut color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..sample_count {
+                let u =
+                    ((i as f64) + (between.sample(&mut rng) as f64)) / ((image_width - 1) as f64);
+                let v =
+                    ((j as f64) + (between.sample(&mut rng) as f64)) / ((image_height - 1) as f64);
+
+                color += scene.process_ray(&camera.get_ray(u, v), 0.0, f64::INFINITY);
+            }
+
+            write_color(color, sample_count);
         }
     }
 }
